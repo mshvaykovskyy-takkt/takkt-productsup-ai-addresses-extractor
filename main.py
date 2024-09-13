0846e3e5e9a6a6351a932289d29a7ab04b506135
mshvaykovskyy-takkt <maksym.shvaykovskyy@takkt.com>
2024-09-13 13:51:00 +0200

    Fix main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import connector.data_service
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        sys.exit(1)
-
-    if len(sys.argv) > 2 and sys.argv[2] == "--health-check":
+    if len(sys.argv) > 1 and sys.argv[1] == "--health-check":
         sys.exit()
 
     connector.data_service.extract_addresses()
